0bfe083d80ac338f11980ad7ba1e0edbd88e3c6a
BrandonP1611 <brandon.perez1611@gmail.com>
2025-05-09 23:37:07 -0700

    Update test_problem_1.py

--- test_problem_1.py
+++ test_problem_1.py
@@ -1,12 +1,12 @@
 def is_power_balanced(n, party_affiliations):
-    # Count how many legislators belong to each party
+    # Counting how many legislators belong to each party
     counts = {1: 0, 2: 0, 3: 0}
     party_list = list(map(int, party_affiliations.split()))
     
     for party in party_list:
         counts[party] += 1
 
-    # Check each party to see if it dominates
+    # Checking each party to see if it dominates
     if counts[1] > counts[2] + counts[3]:
         return "Future One Dominates"
     elif counts[2] > counts[1] + counts[3]:
